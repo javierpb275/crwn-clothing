f4f918ef0b0bfd7c8ee09c67c3c661d1c0fd31b7
javierpb275 <62296694+javierpb275@users.noreply.github.com>
2020-04-16 19:59:36 +0200

    Redux set up

---
+++ src/redux/root-reducer.js
@@ -0,0 +1,32 @@
+import { combineReducers } from 'redux';
+
+import userReducer from './user/user.reducer';
+
+
+export default combineReducers({
+    user: userReducer
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
